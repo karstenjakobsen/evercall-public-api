0f5b356b363ebc3a0d28cee4b7d2f0a87d42373c
Karsten Jakobsen <kj@evercall.dk>
2017-01-20 19:33:18 +0100

    Init

---
+++ src/SayHello.php
@@ -0,0 +1,13 @@
+<?php
+namespace Evercall;
+
+/**
+ * Class SayHello
+ * @package Evercall
+ */
+class SayHello {
+
+	public static function HelloWorld() {
+		return "Hello world!";
+	}
+}
---
+++ tests/test.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+echo \Evercall\SayHello::HelloWorld();
+echo \Evercall\SayHi::Hi();
